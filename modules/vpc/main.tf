dc30c7df9abd29a34833c74e8f58142643d9c5e3
Nehemiah Onyekachukwu Emmanuel <emmanueln644@gmail.com>
2026-01-16 13:19:31 +0100

    Fix duplicate output definitions in vpc module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,19 +1,22 @@
 resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
-  tags = { Name = "eks-vpc", "kubernetes.io/cluster/devgenix-eks-prod" = "shared" }
+
+  tags = {
+    Name = "main-vpc"
+  }
 }
 
 resource "aws_subnet" "public" {
-  count             = 2
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.${count.index}.0/24"
-  availability_zone = data.aws_availability_zones.available.names[count.index]
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
-  tags = { 
-    Name = "public-${count.index}",
-    "kubernetes.io/cluster/devgenix-eks-prod" = "shared",
+
+  tags = {
+    Name                     = "public-subnet-${count.index}"
     "kubernetes.io/role/elb" = "1"
   }
 }
@@ -21,25 +24,17 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   count             = 2
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.${count.index + 10}.0/24"
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index + 2)
   availability_zone = data.aws_availability_zones.available.names[count.index]
-  tags = { 
-    Name = "private-${count.index}",
-    "kubernetes.io/cluster/devgenix-eks-prod" = "shared",
+
+  tags = {
+    Name                              = "private-subnet-${count.index}"
     "kubernetes.io/role/internal-elb" = "1"
   }
 }
 
 data "aws_availability_zones" "available" {}
 
-resource "aws_internet_gateway" "gw" { vpc_id = aws_vpc.main.id }
-
-resource "aws_eip" "nat" { vpc = true }
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public[0].id
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
 }
-
-output "vpc_id" { value = aws_vpc.main.id }
-output "public_subnets" { value = aws_subnet.public[*].id }
-output "private_subnets" { value = aws_subnet.private[*].id }
